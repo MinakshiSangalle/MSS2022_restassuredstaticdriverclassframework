9a4f6188c4e9bb9558f0c5e58a5e5bf1aea5a7aa
Minakshi <Minakshi@DESKTOP-NVU3C2H>
2023-05-25 17:54:38 +0530

    restassuredstaticdriverclass

---
+++ src/driverclass.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+import testClass.Post_TC_1;
+
+public class driverclass {
+
+	public static void main(String[] args) throws IOException {
+		
+		Post_TC_1.execute();
+		
+
+	}
+
+}
